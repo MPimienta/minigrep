acdb442f40c31b1d0d0808be189a11c9e4061e44
MeaBurro47 <mauriciopimienta.f@gmail.com>
2024-10-14 01:04:45 +0200

    Separeted into two modules and added Aplication Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use core::panic;
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 
 
@@ -12,30 +12,18 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
-
-}
-
-struct Config{
-    query: String,
-    file_path:String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    
+    if let Err(e) = minigrep::run(config){
+        println!("Application error: {e}");
+        process::exit(1);
     }
+
 }
+
+
 
 
 
